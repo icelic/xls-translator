62f768d20854f526937e3d2d4bc045a2a1b55d3e
icelic <icelic2@gmail.com>
2018-02-06 12:02:48 +0100

    Added command line arguments and fixed % output.

--- xls-translate.py
+++ xls-translate.py
@@ -12,11 +12,16 @@ try:
 except ImportError:
     print("Please install xlwt package.")
 
-if len(sys.argv) != 2:
-    print("Expecting filename. Got: '%s'"
+if len(sys.argv) != 6:
+    print("Arguments should be: filename translate_from_col translate_to_col translate_from_row translate_to_row. Got: '%s'"
           % ",".join(map(str, sys.argv)))
+    sys.exit()
 else:
     fname = sys.argv[1]
+    source_col = int(sys.argv[2])
+    result_col = int(sys.argv[3])
+    start_row = int(sys.argv[4])
+    end_row = int(sys.argv[5])
 
 workbook = xlrd.open_workbook(fname, encoding_override="cp1252")
 
@@ -25,14 +30,10 @@ wb = Workbook(encoding='utf-8')
 w_sheet = wb.add_sheet('test.xls')
 f = open('translation_transcript.txt', 'a')
 
-for i in range(40000, 55975):
-    word = sheet.cell(i, 2).value
-    try:
-        pass
-    except Exception as e:
-        raise e
-    finally:
-        pass
+
+
+for i in range(start_row, end_row):
+    word = sheet.cell(i, source_col).value
     try:
         r = requests.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180123T162008Z.300eb657ff535043.c62444d79cf78f5576dde55e49d0c6fb1473ca3e&text=" + str(word) + "&lang=hr&format=plain")
     except Exception as e:
@@ -40,16 +41,21 @@ for i in range(40000, 55975):
     finally:
         pass
 
-    first_bracket = r.content.index("[")+2
-    second_bracket = r.content.index("]")-1
+    # this part is custom logic bnecause of structure that yandex returns
+    first_bracket = r.content.find("[") + 2
+    second_bracket = r.content.find("]") - 1
     content = r.content
 
     content = content[first_bracket:second_bracket]
+    # end logic
 
     print content
-    w_sheet.write(i, 3, content)
+    # writes to separate file because of encoding error
+    # TODO make it work in the same file
+    w_sheet.write(i, result_col, content)
     f.write(content + "\n")
-    print "Row:" + str(i) + "; " + str(i*100/55975.0) + "%"
+    print "Row:" + str(i) + "; " \
+        + str((i-start_row)*100/float(end_row-start_row)) + "%"
 
 f.close()
 wb.save('translated.xls')
